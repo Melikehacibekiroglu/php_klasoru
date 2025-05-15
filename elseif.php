efa7ea060e83717f5950f4be2f0f04153e2c087e
Melikehacibekiroglu <202203011019@dogus.edu.tr>
2025-05-15 15:56:43 +0300

    2up to date

---
+++ elseif.php
@@ -0,0 +1,18 @@
+<?php
+$not=72;
+if($not>=90){
+    echo "notunuz AA";
+}
+elseif( $not >=80){
+echo"notunuz BA";
+}
+elseif($not>=70){
+    echo"notunuz BB";
+}
+elseif($not>=60){
+    echo"notunuz CC";
+}
+else{
+    echo "kaldınız, FF";
+}
+?>

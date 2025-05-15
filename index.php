499f0e8232c68129bde7d5a2e136682c8b676565
Melikehacibekiroglu <202203011019@dogus.edu.tr>
2025-05-15 15:38:41 +0300

    first update

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$ad = "Zeynep";       
+$yas = 22;            
+$boy = 1.68;          
+$ogrenciMi = true;    
+
+echo "Ad: $ad <br>";
+echo "Yaş: $yas <br>";
+echo "Boy: $boy <br>";
+echo "Öğrenci mi? " . ($ogrenciMi ? "Evet" : "Hayır");
+?>
